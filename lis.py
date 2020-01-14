33c01d6e1dcf07d95f5f9e0ee73512b80862dd16
Elman Reasat <elmanreasat@hotmail.com>
2020-01-14 10:58:40 -0500

    renamed lisp.py to lis.py

---
+++ lis.py
@@ -0,0 +1,16 @@
+from evaluate import *
+
+def repl(prompt='lisp.py> '):
+        while True:
+            val = eval(parse(input(prompt)))
+            if val is not None: 
+                print(schemestr(val))
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
+
+repl()
